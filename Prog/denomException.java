43c50fe119543956d64763aa8487648734c561ff
tiredisnotavailable <tiredisnotavailable@gmail.com>
2020-06-07 19:39:51 +0300

    first commit

---
+++ Prog/denomException.java
@@ -0,0 +1,11 @@
+package Software.Prog;
+
+public class denomException extends Exception
+{
+    private static final long serialVersionUID = 1L;
+
+    public denomException(String message)
+    {
+        super(message);
+    }
+}
